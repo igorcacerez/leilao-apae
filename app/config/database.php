18f0aad9adf11522f012a1f9c569aac0a2b8f995
Edilson Pereira <edilson.pereira.iami@gmail.com>
2020-11-17 17:54:07 -0300

    Tema do site

--- app/config/database.php
+++ app/config/database.php
@@ -1,8 +1,8 @@
 <?php
 
 $database = [
-    'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => ''
+    'hostname' => '192.185.216.95',
+    'username' => 'desigual_apae',
+    'password' => 'suporte123',
+    'database' => 'desigual_apae'
 ];
---
+++ app/views/site/include/header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="pt-br">
+
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Norda - Minimal eCommerce HTML Template</title>
+    <meta name="robots" content="noindex, follow" />
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="<?= BASE_URL; ?>assets/theme/site/images/favicon.png">
+
+    <link rel="stylesheet" href="<?= BASE_URL; ?>assets/theme/site/css/vendor/vendor.min.css">
+    <link rel="stylesheet" href="<?= BASE_URL; ?>assets/theme/site/css/plugins/plugins.min.css">
+    <link rel="stylesheet" href="<?= BASE_URL; ?>assets/theme/site/css/style.min.css">
+
+</head>
+
+<body>
