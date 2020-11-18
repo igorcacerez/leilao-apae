c9d3cf4b4759cf05a56e5a492fe61fc20db2f63e
igorcacerez <igor.cacerez@gmail.com>
2020-11-18 09:16:10 -0300

    Rotas da api

--- app/config/rotas.php
+++ app/config/rotas.php
@@ -5,11 +5,42 @@ $Rotas->onError("404", function (){
    echo "Erro - 404";
 });
 
-// -- Seta os grupos
-//$Rotas->group("Site","api","Site");
+/**
+ * ========================================================
+ *                      ROTAS DA API
+ * ========================================================
+ */
 
-// -- Rotas de Grupos
-//$Rotas->onGroup("Site","GET","a","index");
+
+// Usuário
+$Rotas->group("api-usuario","api/usuario","Api\Usuario");
+$Rotas->onGroup("api-usuario","POST","login","login");
+$Rotas->onGroup("api-usuario","POST","insert","insert");
+$Rotas->onGroup("api-usuario","PUT","update/{p}","update");
+$Rotas->onGroup("api-usuario","DELETE","delete/{p}","delete");
+
+
+// Produto
+$Rotas->group("api-produto","api/produto","Api\Produto");
+$Rotas->onGroup("api-produto","POST","insert","insert");
+$Rotas->onGroup("api-produto","POST","update/{p}","update");
+$Rotas->onGroup("api-produto","DELETE","delete/{p}","delete");
+
+
+// Imagem
+$Rotas->group("api-imagem","api/imagem","Api\Imagem");
+$Rotas->onGroup("api-imagem","POST","insert/{p}","insert");
+$Rotas->onGroup("api-imagem","PUT","principal/{p}","principal");
+$Rotas->onGroup("api-imagem","DELETE","delete/{p}","delete");
+
+
+
+/**
+ * ========================================================
+ *                      ROTAS DO SITE
+ * ========================================================
+ */
+
 
 // -- Rotas sem grupo
 $Rotas->on("GET","","Site::index");
